308dc78d580d987fc4df407de5ae91b0f0643176
flourvic <flourvic@student.21-school.ru>
2025-03-14 02:47:15 +0700

    pong_field

--- src/main.go
+++ src/main.go
@@ -1,6 +1,26 @@
-package main 		
-import "fmt"
+package main
+
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Print("go")
+	height := 25
+	width := 80
+	field(height, width)
+}
+
+func field(height, width int) {
+	borderChar := "#"
+	emptyChar := " "
+	fmt.Println(strings.Repeat(borderChar, width))
+	for i := 0; i < height-2; i++ {
+		fmt.Println(borderChar + strings.Repeat(emptyChar, width-2) + borderChar)
+	}
+	fmt.Println(strings.Repeat(borderChar, width))
+}
+
+func ball() {
+
 }
