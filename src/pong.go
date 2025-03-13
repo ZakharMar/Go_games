e456c3dcaa9e59a7001cbf70f53999d368bfbc85
flourvic <flourvic@student.21-school.ru>
2025-03-14 03:39:34 +0700

    full_pole

--- src/pong.go
+++ src/pong.go
@@ -8,19 +8,45 @@ import (
 func main() {
 	height := 25
 	width := 80
-	field(height, width)
+
+	ballX, ballY := width/2, height/2
+	leftPaddleY := height/2 - 1
+	rightPaddleY := height/2 - 1
+	paddleHeight := 3
+
+	game(height, width, ballX, ballY, leftPaddleY, rightPaddleY, paddleHeight)
 }
 
-func field(height, width int) {
+func field(height, width, ballX, ballY, leftPaddleY, rightPaddleY, paddleHeight int) {
 	borderChar := "#"
 	emptyChar := " "
+	ballChar := "O"
+	paddleChar := "|"
+
 	fmt.Println(strings.Repeat(borderChar, width))
-	for i := 0; i < height-2; i++ {
-		fmt.Println(borderChar + strings.Repeat(emptyChar, width-2) + borderChar)
+
+	for y := 0; y < height-2; y++ {
+		line := borderChar
+		for x := 0; x < width-2; x++ {
+			if x == ballX && y == ballY {
+				line += ballChar
+			} else if x == 1 && y >= leftPaddleY && y < leftPaddleY+paddleHeight {
+				line += paddleChar
+			} else if x == width-4 && y >= rightPaddleY && y < rightPaddleY+paddleHeight {
+				line += paddleChar
+			} else {
+				line += emptyChar
+			}
+		}
+		line += borderChar
+		fmt.Println(line)
 	}
 	fmt.Println(strings.Repeat(borderChar, width))
 }
 
-func ball() {
-
+func game(height, width, ballX, ballY, leftPaddleY, rightPaddleY, paddleHeight int) {
+	for {
+		fmt.Print("\033[H\033[2J")
+		field(height, width, ballX, ballY, leftPaddleY, rightPaddleY, paddleHeight)
+	}
 }
